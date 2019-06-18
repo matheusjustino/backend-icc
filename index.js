0fb31750344ab65329772ca2db1bbcd58a902097
matheusjustino <matheus.justino@ccc.ufcg.edu.br>
2019-06-18 17:29:25 -0300

    config

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cors = require("cors");
 const url = "mongodb+srv://deploy:dbicc321@cluster0-r58uk.mongodb.net/test?retryWrites=true&w=majority";
 const db_n = "dbicc";
 
-let database, collection;
+let database, collectionAulas, collectionMatriculas;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -30,7 +30,7 @@ app.use(express.json(), function (req, res, next) {
 
 app.post('/salvaValores', (req, res) => {
     console.log(req.body);
-    collection.insert(req.body, (error, result) => {
+    collectionAulas.insert(req.body, (error, result) => {
         if (error) {
             console.log(error);
         }
@@ -44,20 +44,31 @@ app.get('/pegaValores', (req, res) => {
     const t = req.query.data;
     console.log(t);
 
-    collection.find({ data: t }).toArray().then(itens => {
-        console.log(itens)
+    collectionAulas.find({ data: t }).toArray().then(itens => {
         const a = solve(itens)
-        console.log(a);
         res.send(a);
     });
 });
 
+app.get('/pegaMatricula', (req, res) => {
+    const x = req.query.matricula;
+    console.log(req.query);
+    
+    collectionMatriculas.find({ matricula: x }).toArray().then(item => {
+        console.log(item);
+        const contemMatricula = item.length > 0 ? true : false;
+        res.send(contemMatricula);
+    });
+});
 
+
+//"mongodb://localhost:27017/"
 app.listen((process.env.PORT || 9000), () => {
     MongoClient.connect(url, { useNewUrlParser: true }, (error, client) => {
         if (error) {throw error;}
         database = client.db(db_n);
-        collection = database.collection('aulas');
+        collectionAulas = database.collection('aulas');
+        collectionMatriculas = database.collection('matriculas');
         console.log("Conectado ao banco de dados: " + db_n + ".");
     });
     console.log("SERVER ON");
@@ -66,53 +77,41 @@ app.listen((process.env.PORT || 9000), () => {
 
 
 function solve(itens) {
+    
     let obj = [
         [0, 0, 0, 0],
         [0, 0, 0, 0],
         [0, 0, 0, 0],
         [0, 0, 0, 0],
-        [0, 0, 0, 0]
+        [0, 0, 0, 0],
+        []
     ]
-    //console.log("555")
-    //console.log(itens);
 
     if (itens.length > 0) {
         for (let i = 0; i < itens.length; i++) {
+            if (itens[i].textArea.length > 0) {
+                let coments = {
+                    id: 0,
+                    matricula: '',
+                    comentario: ''
+                };
+                coments.id = i; coments.matricula = itens[i].matricula; coments.comentario = itens[i].textArea;
+                obj[5].push(coments);
+            }
             for (let j = 0; j < itens[i].values.length; j++) {
                 if (itens[i].values[j] === 1) {
                     obj[0][j] += 1;
-                }
-            }
-        }
-        for (let i = 0; i < itens.length; i++) {
-            for (let j = 0; j < itens[i].values.length; j++) {
-                if (itens[i].values[j] === 2) {
+                } else if (itens[i].values[j] === 2) {
                     obj[1][j] += 1;
-                }
-            }
-        }
-        for (let i = 0; i < itens.length; i++) {
-            for (let j = 0; j < itens[i].values.length; j++) {
-                if (itens[i].values[j] === 3) {
+                } else if (itens[i].values[j] === 3) {
                     obj[2][j] += 1;
-                }
-            }
-        }
-        for (let i = 0; i < itens.length; i++) {
-            for (let j = 0; j < itens[i].values.length; j++) {
-                if (itens[i].values[j] === 4) {
+                } else if (itens[i].values[j] === 4) {
                     obj[3][j] += 1;
-                }
-            }
-        }
-        for (let i = 0; i < itens.length; i++) {
-            for (let j = 0; j < itens[i].values.length; j++) {
-                if (itens[i].values[j] === 5) {
+                } else if (itens[i].values[j] === 5) {
                     obj[4][j] += 1;
                 }
             }
-        }                                                                                                                                                                                                                                                                                                           
+        }
     }
-
     return obj;
 }
